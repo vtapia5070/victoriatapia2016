58e2c4ce23642312330d1c58f0de8927a4314999
Victoria <tapia.victoria5070@gmail.com>
2015-10-18 21:23:29 -0700

    edited routes/index.js to get static files

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/home', routes);
+app.use('/config.js', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
