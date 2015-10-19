f578b4d841dd6f6cc268b90855ff850dccaaa243
Victoria <tapia.victoria5070@gmail.com>
2015-10-18 20:59:31 -0700

    changed static path to '/public' in app.js

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use('/home', routes);
 app.use('/users', users);
 
